0be1bc4705ce2b337fab83bd392e8144f68deac4
Cèsar Ordiñana <cordin@disid.com>
2017-08-18 08:28:01 +0200

    Replace the in memory authentication with a new one using the new AdminUser entity

--- src/main/java/com/disid/proof/ldap/config/WebSecurityConfig.java
+++ src/main/java/com/disid/proof/ldap/config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.disid.proof.ldap.config;
 
+import com.disid.proof.ldap.service.api.AdminUserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 import java.util.Arrays;
@@ -14,6 +20,12 @@ import java.util.Arrays;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
+  /**
+   * Administration Users service to obtain user info to check by {@link AuthenticationProvider}'s
+   */
+  @Autowired
+  private AdminUserService adminUserService;
+
 
   @Override
   protected void configure( HttpSecurity http ) throws Exception
@@ -28,8 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     // To have many authentication options, just add them to the AuthenticationManagerBuilder in
     // the order to be checked
 
-    auth.inMemoryAuthentication().withUser( "user" ).password( "password" ).roles( "USER" ).and().withUser( "admin" )
-        .password( "password" ).roles( "USER", "ADMIN" );
+    auth.authenticationProvider( defaultAdminAuthenticationProvider() );
 
     auth.ldapAuthentication().userDnPatterns( "uid={0},ou=people" ).groupSearchBase( "ou=groups" )
         .contextSource( contextSource() ).passwordCompare().passwordEncoder( new LdapShaPasswordEncoder() )
@@ -41,6 +52,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
   {
     return new DefaultSpringSecurityContextSource( Arrays.asList( "ldap://localhost:8389/" ),
         "dc=springframework,dc=org" );
+  }
+
+  /**
+   * Create a default {@link AuthenticationProvider} bean for default CAM administrator.
+   *
+   * @return {@link AuthenticationProvider}
+   */
+  @Bean
+  public AuthenticationProvider defaultAdminAuthenticationProvider()
+  {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService( adminUserService );
+    provider.setPasswordEncoder( new BCryptPasswordEncoder() );
+    return provider;
   }
 
 }
